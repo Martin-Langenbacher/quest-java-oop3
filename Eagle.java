e541b8165c21794928db94bfeea2e65e7d393020
Martin Langenbacher <martin.langenbacher@googlemail.com>
2019-11-26 13:39:19 +0100

    ML ist fertig

--- Eagle.java
+++ Eagle.java
@@ -1,24 +1,93 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
-    private boolean flying;
-    private int altitude;
+	private boolean flying;
+	private int altitude;
 
-    public Eagle(String name) {
-        super(name);
-        this.flying = false;
-        this.altitude = 0;
-    }
+	public Eagle(String name) {
+		super(name);
+		this.flying = false;
+		this.altitude = 0;
+	}
 
-    public int getAltitude() {
-        return altitude;
-    }
+	@Override
+	public String sing() {
+		return "Screech!";
+	}
 
-    public boolean isFlying() {
-        return flying;
-    }
+	public int getAltitude() {
+		return altitude;
+	}
 
-    @Override
-    public String sing() {
-        return "Screech!";
-    }
+	public boolean isFlying() {
+		return flying;
+	}
+
+	/**
+	 * takeOff, must be on ground
+	 */
+	@Override
+	public void takeOff() {
+		if (!this.flying && this.altitude == 0) {
+			this.flying = true;
+			System.out.printf("%s takes off in the sky.%n", this.getName());
+		}
+	}
+
+	/**
+	 * ascend
+	 * 
+	 * @param meters heights increase
+	 * @return 
+	 * @return heights
+	 */
+	@Override	
+	public int ascend(int meters) {
+		if (this.flying) {
+			this.altitude = Math.min(this.altitude + meters, 5000);
+			System.out.printf("%s flies upward, altitude: %d%n", this.getName(), this.altitude);
+		}
+		return this.altitude;
+	}
+
+	/**
+	 * descend
+	 * 
+	 * @param meters heights decrease
+	 * @return heights
+	 */
+	@Override
+	public int descend(int meters) {
+		if (this.flying) {
+			this.altitude = Math.max(this.altitude - meters, 0);
+			System.out.printf("%s flies downward, altitude: %d%n", this.getName(), this.altitude);
+		}
+		return this.altitude;
+	}
+
+	/**
+	 * land, must be on ground
+	 */
+	@Override
+	public void land() {
+		if (this.flying && this.altitude < 10) {
+			System.out.printf("%s lands on the ground.%n", this.getName());
+		} else {
+			System.out.printf("%s is too high, it can't land.%n", this.getName());
+		}
+	}
+
+	/**
+	 * glide, no change in altitude, displaying "glides into the air"
+	 */
+	@Override
+	public void glide() {
+		System.out.println("glides into the air.");
+		
+	}
+		
+
 }
+
+
+
+
